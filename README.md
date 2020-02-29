c7a9f497a764904ffb07a539e5d06cdca15270d5
ivargas1030 <56612683+ivargas1030@users.noreply.github.com>
2020-02-29 17:35:32 -0600

    readme edits

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ![Screenshot of Landing Page](./public/FitnessTracker.JPG)
 
-* This app is tracker to keep track of your workouts!
+* Keep track of your workouts with this awesome Fitness Tracker!
 
 * [Heroku Deployed Link](https://shielded-ocean-69209.herokuapp.com/)
+
+* Take a look behind the scenes at the code that makes the app run. Here is the workout schema:
+* ![Screesnhot of workout.js](./public/FitnessTrackerCode.JPG)
--- models/workout.js
+++ models/workout.js
@@ -18,17 +18,14 @@ const WorkoutSchema = new Schema({
   ]
 });
 
-WorkoutSchema.virtual('totalDuration').get(function() {
-    let total = 0;
-    for (let i = 0; i < this.exercises.length; i++) {
-
-        total = this.exercises[i].duration + total;
-        
-    
-    }
-    return total;
-  });
-  WorkoutSchema.set('toJSON', {virtuals: true})
+WorkoutSchema.virtual("totalDuration").get(function() {
+  let total = 0;
+  for (let i = 0; i < this.exercises.length; i++) {
+    total = this.exercises[i].duration + total;
+  }
+  return total;
+});
+WorkoutSchema.set("toJSON", { virtuals: true });
 
 const Workout = mongoose.model("workout", WorkoutSchema);
 module.exports = Workout;
